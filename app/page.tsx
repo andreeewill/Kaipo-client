b72d537487dfd7ef108f987cc69a5490519dfed5
Andre William <andrewilliam@macbook.local>
2025-04-24 22:09:52 +0700

    Fix shacdn, reconfigure folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <h1 className="text-3xl font-bold underline text-yellow-300">
-      This is a new app ok hilal ganteng
+      This is a new app ok hilal ganteng <Button>andre</Button>
     </h1>
   );
 }
