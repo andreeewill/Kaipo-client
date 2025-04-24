f322cb86f16b8910adc78f5c4af97ee289628e1d
andreeewill <andre.williamyuw@gmail.com>
2025-04-24 22:50:24 +0700

    Fix dummy login page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <h1 className="text-3xl font-bold underline text-yellow-300">
+      This is login page<Button>andre</Button>
+    </h1>
+  );
+}
